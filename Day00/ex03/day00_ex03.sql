8c4a6097fef36c8bf104373354293a321043993b
ccartman <ccartman@student.21-school.ru>
2023-12-24 16:17:31 +0300

    Add ex03

--- Day00/ex03/day00_ex03.sql
+++ Day00/ex03/day00_ex03.sql
@@ -1,5 +1,5 @@
-SELECT DISTINCT person_id FROM person_visits
-WHERE pizzeria_id = 2
-AND visit_date BETWEEN '2022/01/06' AND '2022/01/09'
-ORDER BY person_id DESC;
+SELECT DISTINCT person_id
+  FROM person_visits
+  WHERE (visit_date BETWEEN '2022/01/06' AND '2022/01/09') OR pizzeria_id = 2
+  ORDER BY person_id DESC;
 
