391e0829b3c0400e3ad4298f55605319f50912ec
ccartman <ccartman@student.21-school.ru>
2023-12-25 15:47:34 +0300

    Fix ex05

--- Day00/ex05/day00_ex05.sql
+++ Day00/ex05/day00_ex05.sql
@@ -1,8 +1,9 @@
-SELECT (SELECT name
-          FROM person
-          WHERE person_id = person.id
-       ) AS person_name
-  FROM person_order
-  WHERE (menu_id = 13 OR menu_id = 14 OR menu_id = 18) AND
-        order_date = '2022/01/07/';
-
+SELECT (
+    SELECT name
+    FROM person
+    WHERE person.id = person_order.person_id
+) AS person_name
+FROM person_order
+WHERE
+    (menu_id = 13 OR menu_id = 14 OR menu_id = 18)
+    AND order_date = '2022-01-07';
