9e8df766b3ac1ca0e863a5d1e4e9831e72fb61bc
ccartman <ccartman@student.21-school.ru>
2023-12-25 15:50:53 +0300

    Fix ex06

--- Day00/ex06/day00_ex06.sql
+++ Day00/ex06/day00_ex06.sql
@@ -1,20 +1,17 @@
-SELECT (SELECT name
-          FROM person
-          WHERE person_id = person.id
-       ) AS person_name,
-       (CASE
-          WHEN
-            (SELECT name
-               FROM person
-               WHERE person_id = person.id
-            ) = 'Denis'
-          THEN
-            TRUE
-          ELSE
-            FALSE
-          END
-       ) AS check_name
-  FROM person_order
-  WHERE (menu_id = 13 OR menu_id = 14 OR menu_id = 18) AND
-        order_date = '2022/01/07/';
-
+SELECT
+    (
+        SELECT name
+        FROM person
+        WHERE person_id = person.id
+    ) AS person_name,
+    (
+        COALESCE((
+            SELECT name
+            FROM person
+            WHERE person_id = person.id
+        ) = 'Denis', FALSE)
+    ) AS check_name
+FROM person_order
+WHERE
+    (menu_id = 13 OR menu_id = 14 OR menu_id = 18)
+    AND order_date = '2022-01-07';
