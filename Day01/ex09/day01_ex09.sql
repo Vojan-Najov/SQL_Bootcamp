6a3c817c6399558840e0cc3be897c2534c0b43b8
ccartman <ccartman@student.21-school.ru>
2023-12-27 20:18:28 +0300

    Fix ex09

--- Day01/ex09/day01_ex09.sql
+++ Day01/ex09/day01_ex09.sql
@@ -5,7 +5,7 @@ WHERE id NOT IN (SELECT pizzeria_id FROM person_visits);
 SELECT name
 FROM pizzeria
 WHERE NOT EXISTS (
-    SELECT pizzeria_id
+    SELECT pv.pizzeria_id
     FROM person_visits AS pv
     WHERE pv.pizzeria_id = pizzeria.id
 );
