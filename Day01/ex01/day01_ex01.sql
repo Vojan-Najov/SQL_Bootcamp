dbe40710fe5e8cd65f97fa879aef9edb3fbf2ecf
ccartman <ccartman@student.21-school.ru>
2023-12-28 16:43:32 +0300

    Fix ex01

--- Day01/ex01/day01_ex01.sql
+++ Day01/ex01/day01_ex01.sql
@@ -1,11 +1,11 @@
 (
     SELECT name AS object_name
     FROM person
-    ORDER BY name
+    ORDER BY object_name
 )
 UNION ALL
 (
     SELECT pizza_name AS object_name
     FROM menu
-    ORDER BY pizza_name
+    ORDER BY object_name
 );
