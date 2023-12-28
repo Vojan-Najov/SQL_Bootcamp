fc11a611835d09def290793d09090a6d1241d51d
ccartman <ccartman@student.21-school.ru>
2023-12-28 16:38:47 +0300

    Fix ex10

--- Day01/ex10/day01_ex10.sql
+++ Day01/ex10/day01_ex10.sql
@@ -1,3 +1,21 @@
+SELECT
+    (
+        SELECT p.name
+        FROM person AS p
+        WHERE p.id = po.person_id
+    ) AS person_name,
+    m.pizza_name AS pizza_name,
+    (
+        SELECT p.name
+        FROM pizzeria AS p
+        WHERE p.id = m.pizzeria_id
+    ) AS pizzeria_name
+FROM person_order AS po
+INNER JOIN menu AS m
+    ON po.menu_id = m.id
+ORDER BY person_name ASC, pizza_name ASC, pizzeria_name ASC;
+
+/*
 WITH pizza AS (
     SELECT
         menu.id,
@@ -7,6 +25,7 @@ WITH pizza AS (
     INNER JOIN pizzeria
         ON menu.pizzeria_id = pizzeria.id
 )
+
 SELECT
     person.name AS person_name,
     pizza.pizza_name AS pizza_name,
@@ -17,24 +36,4 @@ INNER JOIN pizza
 INNER JOIN person
     ON person_order.person_id = person.id
 ORDER BY person_name ASC, pizza_name ASC, pizzeria_name ASC;
-
-SELECT
-    (
-        SELECT name
-        FROM person
-        WHERE id = po.person_id
-    ) AS person_name,
-    (
-        SELECT pizza_name
-        FROM menu
-        WHERE id = po.menu_id
-    ) AS pizza_name,
-    (
-        SELECT name
-        FROM pizzeria
-        WHERE id = menu.pizzeria_id
-    ) AS pizzeria_name
-FROM person_order AS po
-INNER JOIN menu
-    ON po.menu_id = menu.id
-ORDER BY person_name ASC, pizza_name ASC, pizzeria_name ASC;
+*/
