e687d5d7e0fa508dda0f710f540b4058b5733061
ccartman <ccartman@student.21-school.ru>
2023-12-28 15:38:22 +0300

    Add second version of query

--- Day01/ex10/day01_ex10.sql
+++ Day01/ex10/day01_ex10.sql
@@ -16,4 +16,25 @@ INNER JOIN pizza
     ON person_order.menu_id = pizza.id
 INNER JOIN person
     ON person_order.person_id = person.id
-ORDER BY person_name ASC, pizza_name ASC, pizzeria_name ASC
+ORDER BY person_name ASC, pizza_name ASC, pizzeria_name ASC;
+
+SELECT
+    (
+        SELECT name
+        FROM person
+        WHERE id = po.person_id
+    ) AS person_name,
+    (
+        SELECT pizza_name
+        FROM menu
+        WHERE id = po.menu_id
+    ) AS pizza_name,
+    (
+        SELECT name
+        FROM pizzeria
+        WHERE id = menu.pizzeria_id
+    ) AS pizzeria_name
+FROM person_order AS po
+INNER JOIN menu
+    ON po.menu_id = menu.id
+ORDER BY person_name ASC, pizza_name ASC, pizzeria_name ASC;
