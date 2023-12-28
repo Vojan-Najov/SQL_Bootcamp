7161aea36ef0517daa7d4f3db3666c7241b5da33
ccartman <ccartman@student.21-school.ru>
2023-12-28 20:32:48 +0300

    Fix ex00

--- Day02/ex00/day02_ex00.sql
+++ Day02/ex00/day02_ex00.sql
@@ -1,11 +1,15 @@
-SELECT p.name
+SELECT
+    p.name AS pizzeria_name,
+    p.rating
 FROM pizzeria AS p
 LEFT OUTER JOIN person_visits AS pv
     ON p.id = pv.pizzeria_id
 WHERE pv.pizzeria_id IS NULL;
 
 /*
-SELECT p.name
+SELECT
+    p.name AS pizzeria_name,
+    p.rating
 FROM person_visits AS pv
 RIGHT OUTER JOIN pizzeria AS p
     ON pv.pizzeria_id = p.id
