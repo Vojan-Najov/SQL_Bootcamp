45f6f7de2919d64b0e8839ea786d227b97b0b025
Vojan Najov <serejah.mashichev@gmail.com>
2023-12-30 13:56:11 +0300

    Add day02 ex02

---
+++ Day02/ex02/day02_ex02.sql
@@ -0,0 +1,17 @@
+SELECT
+    COALESCE(person.name, '-') AS person_name,
+    pv.visit_date AS visit_date,
+    COALESCE(pizzeria.name, '-') AS pizzeria_name
+FROM (
+	SELECT
+	    person_id,
+	    pizzeria_id,
+	    visit_date
+	FROM person_visits
+	WHERE visit_date BETWEEN '2022-01-01' AND '2022-01-03'
+      ) AS pv
+FULL OUTER JOIN person
+    ON pv.person_id = person.id
+FULL OUTER JOIN pizzeria
+    ON pv.pizzeria_id = pizzeria.id
+ORDER BY 1 ASC, 2 ASC, 3 ASC;
