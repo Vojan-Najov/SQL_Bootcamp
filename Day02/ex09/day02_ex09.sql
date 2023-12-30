0ef6ee1e9ece41e97c4e35243d8fa72763dc38fe
Vojan Najov <serejah.mashichev@gmail.com>
2023-12-30 18:31:13 +0300

    Add day02 ex09

--- Day02/ex09/day02_ex09.sql
+++ Day02/ex09/day02_ex09.sql
@@ -1,6 +1,5 @@
 (
-    SELECT
-        person.name AS name
+    SELECT person.name AS name
     FROM person_order AS po
     INNER JOIN person
         ON po.person_id = person.id
@@ -8,10 +7,9 @@
         ON po.menu_id = menu.id
     WHERE person.gender = 'female' AND menu.pizza_name = 'pepperoni pizza'
 )
-intersect DISTINCT
+INTERSECT
 (
-    SELECT
-        person.name AS name
+    SELECT person.name AS name
     FROM person_order AS po
     INNER JOIN person
         ON po.person_id = person.id
