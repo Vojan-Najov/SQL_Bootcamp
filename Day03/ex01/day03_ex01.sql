1e9451cd9cc5215ce4d6e6246856e3acc70d4b39
Vojan Najov <serejah.mashichev@gmail.com>
2023-12-31 11:51:19 +0300

    Fix day03 ex01

--- Day03/ex01/day03_ex01.sql
+++ Day03/ex01/day03_ex01.sql
@@ -1,6 +1,15 @@
 SELECT menu.id AS menu_id
+FROM menu
+LEFT OUTER JOIN person_order AS po
+    ON menu.id = po.menu_id
+WHERE po.menu_id IS NULL
+ORDER BY menu.id ASC;
+
+/*
+SELECT menu.id AS menu_id
 FROM person_order AS po
 RIGHT OUTER JOIN menu
     ON po.menu_id = menu.id
 WHERE po.id IS NULL
 ORDER BY menu.id ASC;
+*/
