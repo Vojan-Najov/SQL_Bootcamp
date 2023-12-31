35fc24262657d5a8563ad0f2c5bfc4cdcb7c0762
Vojan Najov <serejah.mashichev@gmail.com>
2023-12-31 14:14:16 +0300

    Add day03 ex03

---
+++ Day03/ex03/day03_ex03.sql
@@ -0,0 +1,33 @@
+WITH visits AS (
+    SELECT
+        pizzeria.name AS pizzeria_name,
+	person.gender AS gender
+    FROM person_visits AS pv
+    INNER JOIN pizzeria
+        ON pv.pizzeria_id = pizzeria.id
+    INNER JOIN person
+        ON pv.person_id = person.id
+),
+visited_by_men AS (
+    SELECT pizzeria_name
+    FROM visits
+    WHERE gender = 'male'
+),
+visited_by_women AS (
+    SELECT pizzeria_name
+    FROM visits
+    WHERE gender = 'female'
+)
+
+(
+    SELECT pizzeria_name FROM visited_by_women
+    EXCEPT ALL
+    SELECT pizzeria_name FROM visited_by_men
+)
+UNION ALL
+(
+    SELECT pizzeria_name FROM visited_by_men
+    EXCEPT ALL
+    SELECT pizzeria_name FROM visited_by_women
+)
+ORDER BY pizzeria_name ASC
