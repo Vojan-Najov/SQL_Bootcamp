0550582155e4a8e593d5e9cf8f72333969c5f139
Vojan Najov <serejah.mashichev@gmail.com>
2023-12-31 15:29:09 +0300

    Fix day03 ex05

--- Day03/ex05/day03_ex05.sql
+++ Day03/ex05/day03_ex05.sql
@@ -1,24 +1,22 @@
 (
-SELECT
-    pizzeria.name AS pizzeria_name
-FROM person_visits AS pv
-INNER JOIN pizzeria
-    ON pv.pizzeria_id = pizzeria.id
-INNER JOIN person
-    ON pv.person_id = person.id
-WHERE person.name = 'Andrey'
+    SELECT pizzeria.name AS pizzeria_name
+    FROM person_visits AS pv
+    INNER JOIN pizzeria
+        ON pv.pizzeria_id = pizzeria.id
+    INNER JOIN person
+        ON pv.person_id = person.id
+    WHERE person.name = 'Andrey'
 )
-EXCEPT DISTINCT
+EXCEPT
 (
-SELECT
-    pizzeria.name AS pizzeria_name
-FROM person_order AS po
-INNER JOIN menu
-    ON po.menu_id = menu.id
-INNER JOIN pizzeria
-    ON menu.pizzeria_id = pizzeria.id
-INNER JOIN person
-    ON po.person_id = person.id
-WHERE person.name = 'Andrey'
+    SELECT pizzeria.name AS pizzeria_name
+    FROM person_order AS po
+    INNER JOIN menu
+        ON po.menu_id = menu.id
+    INNER JOIN pizzeria
+        ON menu.pizzeria_id = pizzeria.id
+    INNER JOIN person
+        ON po.person_id = person.id
+    WHERE person.name = 'Andrey'
 )
 ORDER BY pizzeria_name;
