ee800f19ac796577967703df26a0482f8ee2b94d
Vojan Najov <serejah.mashichev@gmail.com>
2024-01-02 15:38:20 +0300

    Add day04 ex06

---
+++ Day04/ex06/day04_ex06.sql
@@ -0,0 +1,13 @@
+CREATE MATERIALIZED VIEW mv_dmitriy_visits_and_eats AS
+SELECT pizzeria.name AS pizzeria_name
+FROM person_visits AS pv
+INNER JOIN person
+    ON pv.person_id = person.id
+INNER JOIN pizzeria
+    ON pv.pizzeria_id = pizzeria.id
+INNER JOIN menu
+    ON pizzeria.id = menu.pizzeria_id
+WHERE
+    person.name = 'Dmitriy'
+    AND pv.visit_date = '2022-01-08'
+    AND menu.price < 800;
