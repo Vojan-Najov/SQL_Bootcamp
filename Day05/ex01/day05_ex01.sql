9ef73c39e9fb40afbb1addaaf5b907c7cad88f9b
Vojan Najov <serejah.mashichev@gmail.com>
2024-01-04 15:24:22 +0300

    Add day05 ex01

---
+++ Day05/ex01/day05_ex01.sql
@@ -0,0 +1,8 @@
+SET enable_seqscan TO off;
+EXPLAIN ANALYZE
+SELECT
+    menu.pizza_name AS pizza_name,
+    pizzeria.name AS pizzeria_name
+FROM menu
+INNER JOIN pizzeria
+    ON menu.pizzeria_id = pizzeria.id;
