bb5ac16f6871fa00ca6fea6cd6fff42b1cba358b
Vojan Najov <serejah.mashichev@gmail.com>
2024-01-05 16:04:34 +0300

    Fix day05 ex01

--- Day05/ex01/day05_ex01.sql
+++ Day05/ex01/day05_ex01.sql
@@ -1,8 +1,9 @@
 SET enable_seqscan TO off;
+
 EXPLAIN ANALYZE
 SELECT
-    menu.pizza_name AS pizza_name,
-    pizzeria.name AS pizzeria_name
-FROM menu
-INNER JOIN pizzeria
-    ON menu.pizzeria_id = pizzeria.id;
+    m.pizza_name AS pizza_name,
+    p.name AS pizzeria_name
+FROM menu AS m
+INNER JOIN pizzeria AS p
+    ON m.pizzeria_id = p.id;
