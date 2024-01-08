4771fdd68adc47288b8f8a6888a9cffe0a2169a4
Vojan Najov <serejah.mashichev@gmail.com>
2024-01-08 19:31:09 +0300

    Add day07 ex03

---
+++ Day07/ex03/day07_ex03.sql
@@ -0,0 +1,24 @@
+WITH visits_and_orders AS (
+    (
+        SELECT pizzeria.name AS name
+        FROM person_order AS po
+        INNER JOIN menu
+            ON po.menu_id = menu.id
+        INNER JOIN pizzeria
+            ON menu.pizzeria_id = pizzeria.id
+    )
+    UNION ALL
+    (
+        SELECT pizzeria.name AS name
+        FROM person_visits AS pv
+        INNER JOIN pizzeria
+            ON pv.pizzeria_id = pizzeria.id
+    )
+)
+
+SELECT
+    vo.name AS name,
+    COUNT(vo.name) AS total_count
+FROM visits_and_orders AS vo
+GROUP BY vo.name
+ORDER BY total_count DESC, name ASC;
