91f98c6b3f8c08e91e24d093ec09c1a86c69fac1
Vojan Najov <serejah.mashichev@gmail.com>
2024-01-08 21:39:54 +0300

    Fix day07 ex04

--- Day07/ex04/day07_ex04.sql
+++ Day07/ex04/day07_ex04.sql
@@ -1,8 +1,8 @@
 SELECT
     person.name AS name,
-    COUNT(person.name) AS count_of_visits
+    COUNT(*) AS count_of_visits
 FROM person_visits AS po
 INNER JOIN person
     ON po.person_id = person.id
 GROUP BY person.name
-HAVING COUNT(person.name) > 3;
+HAVING COUNT(*) > 3;
