d5af3398b76b122785dd9a49c74eff2c410f3fe6
Vojan Najov <serejah.mashichev@gmail.com>
2024-01-08 21:28:01 +0300

    Fix day07 ex01

--- Day07/ex01/day07_ex01.sql
+++ Day07/ex01/day07_ex01.sql
@@ -1,9 +1,9 @@
 SELECT
     person.name AS name,
-    COUNT(person.name) AS counts_of_visits
+    COUNT(*) AS count_of_visits
 FROM person_visits AS pv
 INNER JOIN person
     ON pv.person_id = person.id
 GROUP BY person.name
-ORDER BY counts_of_visits DESC, person.name ASC
+ORDER BY count_of_visits DESC, person.name ASC
 LIMIT 4;
