136e26d0facfcb87a46fa8f8b044283d7dcf1c07
Vojan Najov <serejah.mashichev@gmail.com>
2024-01-11 00:52:53 +0300

    Fix day09 ex00

--- Day09/ex00/day09_ex00.sql
+++ Day09/ex00/day09_ex00.sql
@@ -16,15 +16,8 @@ CREATE FUNCTION fnc_trg_person_insert_audit() RETURNS trigger
 AS $person_insert_audit$
     BEGIN
         IF (TG_OP = 'INSERT') THEN
-            INSERT INTO person_audit
-	    SELECT
-                current_timestamp,
-                'I',
-                NEW.id,
-                NEW.name,
-                NEW.age,
-                NEW.gender,
-                NEW.address;
+            INSERT INTO person_audit(created, type_event, row_id, name, age, gender, address)
+            VALUES (current_timestamp, 'I', NEW.id, NEW.name, NEW.age, NEW.gender, NEW.address);
         END IF;
         
 	RETURN NULL;
