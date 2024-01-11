9da7ed323f37f581af28d61d0cc178420afd04c9
Vojan Najov <serejah.mashichev@gmail.com>
2024-01-11 11:11:05 +0300

    Fix day09 ex02

--- Day09/ex02/day02_ex02.sql
+++ Day09/ex02/day02_ex02.sql
@@ -1,16 +1,15 @@
 CREATE FUNCTION fnc_trg_person_delete_audit() RETURNS TRIGGER
 AS $person_delete_audit$
     BEGIN
-        IF (TG_OP = 'DELETE') THEN
-            INSERT INTO person_audit(created, type_event, row_id, name, age, gender, address)
-            VALUES (current_timestamp, 'D', OLD.id, OLD.name, OLD.age, OLD.gender, OLD.address);
-        END IF;
+        INSERT INTO person_audit(created, type_event, row_id, name, age, gender, address)
+        VALUES (current_timestamp, 'D', OLD.id, OLD.name, OLD.age, OLD.gender, OLD.address);
 
         RETURN NULL;
     END;
 $person_delete_audit$ LANGUAGE plpgsql;
 
-CREATE TRIGGER trg_person_delete_audit AFTER DELETE ON person
+CREATE TRIGGER trg_person_delete_audit AFTER DELETE
+ON person
 FOR EACH ROW EXECUTE FUNCTION fnc_trg_person_delete_audit();
 
 DELETE FROM person WHERE id = 10;
