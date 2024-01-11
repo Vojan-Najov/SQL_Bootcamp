859c621dffd18b4cf57bb66d7db1f7d2b082f293
Vojan Najov <serejah.mashichev@gmail.com>
2024-01-11 11:06:19 +0300

    Fix day09 ex01

--- Day09/ex01/day09_ex01.sql
+++ Day09/ex01/day09_ex01.sql
@@ -1,17 +1,17 @@
 CREATE FUNCTION fnc_trg_person_update_audit() RETURNS trigger
 AS $person_update_audit$
     BEGIN
-        IF (TG_OP = 'UPDATE') THEN
-            INSERT INTO person_audit(created, type_event, row_id, name, age, gender, address)
-            VALUES (current_timestamp, 'U', OLD.id, OLD.name, OLD.age, OLD.gender, OLD.address);
-        END IF;
+        INSERT INTO person_audit(created, type_event, row_id, name, age, gender, address)
+        VALUES (current_timestamp, 'U', OLD.id, OLD.name, OLD.age, OLD.gender, OLD.address);
 
         RETURN NULL;
     END;
 $person_update_audit$ LANGUAGE plpgsql;
 
-CREATE TRIGGER trg_person_update_audit AFTER UPDATE ON person
+CREATE TRIGGER trg_person_update_audit AFTER UPDATE
+ON person
 FOR EACH ROW EXECUTE FUNCTION fnc_trg_person_update_audit();
 
 UPDATE person SET name = 'Bulat' WHERE id = 10;
+
 UPDATE person SET name = 'Damir' WHERE id = 10;
